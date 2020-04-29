658079bac1d42f4318fcfc8a4c9914876fa567a0
Adrian Salvador <adrian@smartlending.mx>
2020-04-29 15:02:20 -0500

    navigation-react-router-dom

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./homepage.styles.scss"
 import DirectoryComponent from "../../components/directory/directory.component";
 
-const HomePage = () => {
+const HomePage = ({history}) => {
   return (
     <div className="homepage">
       <DirectoryComponent />
