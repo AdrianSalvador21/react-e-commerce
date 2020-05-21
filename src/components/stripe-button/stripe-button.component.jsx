a7c0c50b3363aacce8108b76d705290831513499
Adrian Salvador <adrian@smartlending.mx>
2020-05-21 01:46:10 -0500

    stripe front-end integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_hqeFnOyyPtvLgyooad18ghaR00YmR9CGTO';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Punto de venta'
+      billingAddress
+      shippingAddress
+      image='https://icons-for-free.com/iconfiles/png/512/app+global+ios+media+social+stripe+icon-1320193330258324292.png'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
